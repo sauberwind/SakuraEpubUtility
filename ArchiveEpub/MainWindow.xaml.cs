92f3cbdce9e8ddcedb9b0ac233842590f2568a97
Hal(@sakuraSoftware) <sauber@pluto.dti.ne.jp>
2013-05-29 18:08:03 +0900

    EPU2未対応(modifiedがないと落ちる)

--- ArchiveEpub/MainWindow.xaml.cs
+++ ArchiveEpub/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace ArchiveEpub
 {
@@ -23,6 +13,94 @@ namespace ArchiveEpub
         public MainWindow()
         {
             InitializeComponent();
+
+            //テキストボックスに前回値を入れる
+            ePubDirPathTextBox.Text=Properties.Settings.Default.epubDirDefault;
+            javaPathTextBox.Text=Properties.Settings.Default.javaPathDefault;
+            EpubCheckPathTextBox.Text = Properties.Settings.Default.epubCheckPathDefault;
+
+            UpdateGenerateEpubStatus();
+        }
+
+
+
+        //ファイルがドラッグされてきたらうける
+        private void OnPreviewDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) != null)
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        //ファイルがドロップされたらテキストボックスに表示する
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files != null)
+            {
+                var textBox = sender as TextBox;
+                textBox.Text = files[0];
+            }
+        }
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateGenerateEpubStatus();
+        }
+
+        //EPUB生成ボタンの実行可否を更新する
+        void UpdateGenerateEpubStatus()
+        {
+            bool isEnable = true;   //仮にEPUB生成可能とする
+
+            //Epubにするディレクトリが存在するか？
+            isEnable &=Directory.Exists(ePubDirPathTextBox.Text);
+
+            //Epubチェックを使用するなら
+            if (useEpubCheck.IsChecked == true)
+            {
+                //javaファイルが存在するか
+                isEnable &= File.Exists(javaPathTextBox.Text);
+
+                //ファイル名がjava.exeであるか
+                var javaFileName = Path.GetFileName(javaPathTextBox.Text).ToLower();
+                isEnable &= javaFileName.Equals("java.exe");
+
+                //EpubCheckファイルが存在するか
+                isEnable &= File.Exists(EpubCheckPathTextBox.Text);
+                
+                var epubCheckExt = Path.GetExtension(EpubCheckPathTextBox.Text).ToLower();
+                isEnable &= epubCheckExt.Equals(".jar");
+            }
+            GenerateEpub.IsEnabled = isEnable;
+        
+        }
+
+        //EpubCheckを使用するイベントハンドラ
+        private void useEpubCheck_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateGenerateEpubStatus();
+
+        }
+
+        private void GenerateEpub_Click(object sender, RoutedEventArgs e)
+        {
+            var srcDir = @"C:\Users\saube_000\Desktop\OOPforDogs";
+            var dstFile = @"C:\Users\saube_000\Desktop\test01.epub";
+
+            EpubArchiver.ArchiveEpub(srcDir,dstFile);
+
+            //設定を保存する
+            Properties.Settings.Default.epubDirDefault = ePubDirPathTextBox.Text;
+            Properties.Settings.Default.javaPathDefault = javaPathTextBox.Text;
+            Properties.Settings.Default.epubCheckPathDefault = EpubCheckPathTextBox.Text;
+
+            Properties.Settings.Default.Save();
         }
     }
 }
---
+++ ArchiveEpub/PackageDocument.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ArchiveEpub
+{
+    class PackageDocument
+    {
+        //パッケージドキュメントのUUIDとmodifiedを更新する
+        public static void UpdatePackageDocument(string srcFile)
+        {
+            XNamespace package = "http://www.idpf.org/2007/opf";
+            XNamespace dc = "http://purl.org/dc/elements/1.1/";
+
+
+            //opfファイルを読み込む
+            var doc = XElement.Load(srcFile);
+
+            //identifierをGUIDで書き換える
+            var idNode = doc.Descendants(dc + "identifier").First();
+            var guid = Guid.NewGuid();
+            idNode.Value = guid.ToString();
+
+            //lastmodifiedを書き換える
+            var metaNodes = doc.Descendants(package + "meta");
+            var lastModifiedNode = metaNodes.Where(e => e.Attribute("property").Value == "dcterms:modified").First();
+            var utc = DateTime.UtcNow;
+            lastModifiedNode.Value = (utc.ToString("s") + 'Z');
+
+            //ファイルを上書きする
+            doc.Save(srcFile);
+        }
+    }
+}
